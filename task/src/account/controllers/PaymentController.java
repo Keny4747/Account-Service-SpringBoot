05e4a3617cf3c5f446ba7e9a8fb232bd2a4486c3
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-05-03 11:13:02 -0500

    ...

--- task/src/account/controllers/PaymentController.java
+++ task/src/account/controllers/PaymentController.java
@@ -1,14 +1,20 @@
 package account.controllers;
 
 import account.entity.employee.Employee;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/acct")
+@Validated
 public class PaymentController {
     @PostMapping("/payments")
-    public void addPaymentUser(@RequestBody Employee [] employee){
-
+    public List<Employee> addPaymentUser(@Valid @RequestBody Employee [] employee){
+        return Arrays.stream(employee).toList();
     }
 
     @PutMapping("/payments/")
